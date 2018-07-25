d20c8c95fb8d471183de3891867fffd3bace5e04
mickey english <mickeyeng92@gmail.com>
2018-07-25 23:38:49 +0100

    created contacts component with dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Contact from "./components/contact";
+import Contacts from "./components/contacts";
 import Header from "./components/header";
 
 class App extends Component {
@@ -10,16 +10,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="john Doe"
-            email="Jdoe@gmail.com"
-            phone="555-5555-555"
-          />
-          <Contact
-            name="Laura Silva"
-            email="Laura@gmail.com"
-            phone="555-5445-555"
-          />
+          <Contacts />
         </div>
       </div>
     );
---
+++ src/components/contacts.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Contact from "./contact";
+
+class Contacts extends React.Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "55555-5555-555"
+      },
+      {
+        id: 2,
+        name: "Bob Jones",
+        email: "bobJones@gmail.com",
+        phone: "55545-54455-555"
+      },
+      {
+        id: 3,
+        name: "Lauren Carter",
+        email: "laurenCarter@gmail.com",
+        phone: "544545-54455-555"
+      }
+    ]
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <div>
+        {contacts.map((contact, id) => <Contact key={id} contact={contact} />)}
+      </div>
+    );
+  }
+}
+
+export default Contacts;
