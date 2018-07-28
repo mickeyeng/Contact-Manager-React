2b2dddcb68ad48b89a5c90da16fa2940272a72f4
mickey english <mickeyeng92@gmail.com>
2018-07-28 21:59:43 +0100

    created contact form with refs instead of state

--- src/components/contacts/addContact1.js
+++ src/components/contacts/addContact1.js
@@ -1,34 +1,26 @@
 import React from "react";
 
 class AddContact extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    name: "",
+    email: "",
+    phone: ""
+  };
 
-    this.nameInput = React.createRef();
-    this.emailInput = React.createRef();
-    this.phoneInput = React.createRef();
-  }
+  onChange = e => {
+    this.setState({
+      // this will get the name of each input
+      [e.target.name]: e.target.value
+    });
+  };
+
   onSubmit = e => {
     e.preventDefault();
     console.log(this.state);
-
-    const contact = {
-      name: this.nameInput.current.value,
-      email: this.emailInput.current.value,
-      phone: this.phoneInput.current.value
-    };
-
-    console.log(contact);
-  };
-
-  static defaultProps = {
-    name: "Fred Smith",
-    email: "fred@gmail.com",
-    phone: "77777-7777777-77"
   };
 
   render() {
-    const { name, email, phone } = this.props;
+    const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
@@ -41,8 +33,8 @@ class AddContact extends React.Component {
                 placeholder="Enter Name..."
                 name="name"
                 type="text"
-                defaultValue={name}
-                ref={this.nameInput}
+                value={name}
+                onChange={this.onChange}
               />
             </div>
 
@@ -53,8 +45,8 @@ class AddContact extends React.Component {
                 placeholder="Enter Email..."
                 name="email"
                 type="email"
-                defaultValue={email}
-                ref={this.emailInput}
+                value={email}
+                onChange={this.onChange}
               />
             </div>
 
@@ -64,8 +56,8 @@ class AddContact extends React.Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Phone Number..."
                 name="phone"
-                defaultValue={phone}
-                ref={this.phoneInput}
+                value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
